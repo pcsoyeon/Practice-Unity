ca14e019c74e7eae6e302be3e1698ea705d34ab9
so yeon <alicesysy@swu.ac.kr>
2021-09-06 16:41:10 +0900

    [feat] 오브젝트 vector 위치 이동

---
+++ tutorial#1/Assets/ObjectTransform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTransform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Transform tr; // 이미 있으므로 굳이 만들 필요 없음
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update는 1초에 50-60번씩 호출되므로 계속 위치 변화 
+        Vector3 vector3 = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.Translate(vector3);
+    }
+}
