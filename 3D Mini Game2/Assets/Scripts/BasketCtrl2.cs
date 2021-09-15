8ec05310d970706bde8eee2b7e4808bfa51067da
so yeon <alicesysy@swu.ac.kr>
2021-09-15 19:54:44 +0900

    [feat] item 충돌 처리

---
+++ 3D Mini Game2/Assets/Scripts/BasketCtrl2.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketCtrl2 : MonoBehaviour
+{
+    public AudioClip correctSnd;
+    public AudioClip wrongSnd;
+    AudioSource ads;
+
+    void Start()
+    {
+        ads = GetComponent<AudioSource>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (transform.position.x > -1)
+            {
+                transform.Translate(-1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (transform.position.x < 1)
+            {
+                transform.Translate(1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (transform.position.z < 1)
+            {
+                transform.Translate(0, 0, 1);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (transform.position.z > -1)
+            {
+                transform.Translate(0, 0, -1);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Apple")
+        {
+            ads.PlayOneShot(correctSnd);
+            Destroy(coll.gameObject);
+        }
+        else if (coll.gameObject.tag == "Bomb")
+        {
+            ads.PlayOneShot(wrongSnd);
+            Destroy(coll.gameObject);
+        }
+    }
+}
---
+++ 3D Mini Game2/Assets/Scripts/ItemGen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGen : MonoBehaviour
+{
+    public GameObject apple;
+    public GameObject bomb;
+
+    float curTime = 0;
+
+    void Update()
+    {
+        curTime += Time.deltaTime;
+
+        if (curTime > 1.5f)
+        {
+            // 오브젝트 생성
+            GameObject obj = (Random.Range(0, 10) > 2) ? Instantiate(apple) : Instantiate(bomb);
+
+            // 랜덤 위치 지정
+            // 범위에서 2는 포함 x 
+            Vector3 objPos = new Vector3(Random.Range(-1, 2), 4, Random.Range(-1, 2));
+
+            obj.transform.position = objPos;
+
+            Destroy(obj, 5); // 5초 후 제거
+            curTime = 0; // 시간 초기화 -> 1.5초마다 아이템 생성
+        }
+    }
+}
