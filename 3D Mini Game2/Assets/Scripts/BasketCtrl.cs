9b93c496018bb2857e2fc8b2aa59953f9b37b4f6
so yeon <alicesysy@swu.ac.kr>
2021-09-15 19:18:18 +0900

    [feat] basket 움직임 구현

---
+++ 3D Mini Game2/Assets/Scripts/BasketCtrl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketCtrl : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (transform.position.x > -1)
+            {
+                transform.Translate(-1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (transform.position.x < 1)
+            {
+                transform.Translate(1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (transform.position.z < 1)
+            {
+                transform.Translate(0, 0, 1);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (transform.position.z > -1)
+            {
+                transform.Translate(0, 0, -1);
+            }
+        }
+    }
+}
