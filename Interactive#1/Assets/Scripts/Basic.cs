22aa6f808e910b394ba0bcd6d667e514a68884d6
so yeon <alicesysy@swu.ac.kr>
2021-09-22 19:07:56 +0900

    [feat] 스테이지 제작

---
+++ Interactive#1/Assets/Scripts/Basic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basic : MonoBehaviour
+{
+    public int num;
+    public float real;
+    public bool fact;
+    public string msg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("num: " + num);
+        print("real: " + real);
+        print("fact: " + fact);
+        print("msg: " + msg);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print("매 프레임 실행");
+    }
+}
