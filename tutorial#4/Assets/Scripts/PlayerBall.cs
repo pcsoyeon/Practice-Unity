442cca374cd10e14ab89a81a290b25163205d9b0
so yeon <alicesysy@swu.ac.kr>
2021-09-26 02:34:19 +0900

    [feat] player 움직임 구현

---
+++ tutorial#4/Assets/Scripts/PlayerBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBall : MonoBehaviour
+{
+    public float jumpPower;
+    Rigidbody rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal"); // GetAxisRaw : 0, 1, -1 로
+        float v = Input.GetAxisRaw("Vertical");
+
+        rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+    }
+}
