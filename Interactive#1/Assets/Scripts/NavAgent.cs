2a886cd83ee8a6479f824cf4ffbc980b5921b655
so yeon <alicesysy@swu.ac.kr>
2021-10-07 01:52:40 +0900

    [add] 프로젝트 추가

---
+++ Interactive#1/Assets/Scripts/NavAgent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgent : MonoBehaviour
+{
+    public Transform desTr;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        // 유효한 경우일 때만 
+        if (agent.isPathStale == false)
+        {
+            agent.destination = desTr.position;
+        }
+    }
+}
